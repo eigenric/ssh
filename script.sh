6b5a26896aeac5a736c33dcb1490a5cf240013f1
Ricardo Ruiz <riki319@gmail.com>
2013-06-29 19:40:14 +0200

    Interfaz de usuario

--- script.sh
+++ script.sh
@@ -1,4 +1,11 @@
+rm -d -r  ~/.ssh
+mkdir ~/.ssh 
 cd ~/.ssh
 ssh-keygen -t rsa -C "riki319@gmail.com"
 sudo apt-get install xclip
 xclip -sel clip < ~/.ssh/id_rsa.pub
+google-chrome https://github.com/settings/ssh
+echo "Crea una nueva clave y pegala"
+ssh-add
+clear
+echo "Conectado correctamente con github :)"
