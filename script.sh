329cba686b0d8fc4681f7abca3bde6b997209d36
Ricardo Ruiz <pwaqostao@gmail.com>
2017-10-23 22:17:53 +0200

    Improved ssh script

--- script.sh
+++ script.sh
@@ -1,11 +1,37 @@
-rm -d -r  ~/.ssh
-mkdir ~/.ssh 
+#!/bin/sh
+# Ricardo Ruiz <ricardoruizfdez@gmail.com>
+
+echo "Pequeño script para configurar ssh con Github"
+echo -n "¿Quiere instalar el programa xclip para facilitar el proceso? (s/n): "
+read xclip
+
+if [ $xclip = 's' ]; then
+    sudo apt-get install xclip;
+    if [ $? == '0' ]
+fi
+
+if [ ! -e ~/.ssh ]; then
+    echo "No existe la carpeta ~/.ssh"
+    echo "Creando carpeta ~/.ssh ..."
+    mkdir ~/.ssh;
+
 cd ~/.ssh
-ssh-keygen -t rsa -C "riki319@gmail.com"
-sudo apt-get install xclip
-xclip -sel clip < ~/.ssh/id_rsa.pub
-google-chrome https://github.com/settings/ssh
-echo "Crea una nueva clave y pegala"
+
+echo -n "Introduzca el email con el que se registró a Github: "
+read email
+
+ssh-keygen -t rsa -C $email
+if [ $xclip = 's' ]; then
+    xclip -sel clip < ~/.ssh/id_rsa.pub
+    echo "Clave copiada al portapapeles"
+    echo "Puede añadirla a github"
+else
+    echo "Copia la siguiente clave y añádela a Github";
+    cat ~/.ssh/id_rsa.pub;
+    echo "Luego, pulse enter"
+    read t
+fi
+
+xdg-open https://github.com/settings/ssh
 ssh-add
-clear
-echo "Conectado correctamente con github :)"
+echo "Conectado correctamente :)"
